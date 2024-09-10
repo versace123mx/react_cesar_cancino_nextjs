b81b4afde60bad8da197a91a3eb6769aec077bef
Gustavo <versace123mx@gmail.com>
2024-09-10 17:32:42 -0600

    categorias y slug

--- pages/categoria/[slug].js
+++ pages/categoria/[slug].js
@@ -3,95 +3,50 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Sidebar from "@/componentes/Sidebar";
 import Aviso from "@/componentes/Aviso";
+import { getCategorias, getAvisosPorCategoria, getCategoriasPorId } from "@/servicios/APIRest";
 
-
-const AvisosPorCategoria = () => {
+const AvisosPorCategoria = ({ categorias, avisos, cat, handleEstaLogueado, authNombre, handleCerrarSesion }) => {
     const router = useRouter()
     console.log(router.query.slug)//aqui se recupera con el nombre del archivo en este caso slug, solo un parametro de la url
     return (
         <>
-            <Frontend title={'Avisos por Categoria'}>
+            <Frontend title={'Categorías'} authNombre={authNombre} handleCerrarSesion={handleCerrarSesion}>
                 <section className="py-5">
                     <div className="container py-5">
                         <div className="row gy-5">
-                            {<Sidebar valor={``} categorias={''} />}
+                            <Sidebar valor={cat.id} categorias={categorias} />
                             <div className="col-lg-9 order-1 order-lg-2">
                                 <nav aria-label="breadcrumb">
                                     <ol className="breadcrumb">
                                         <li className="breadcrumb-item">
                                             <Link href="/">Home</Link>
                                         </li>
+                                        <li className="breadcrumb-item">
+                                            <Link href="/categorias">Categorías</Link>
+                                        </li>
                                         <li className="breadcrumb-item active" aria-current="page">
-                                            Categorías
+                                            Categorías: {cat.nombre}
                                         </li>
                                     </ol>
                                 </nav>
-
+                                <div className="row mb-4 align-items-center">
+                                    <div className="col-md-7"></div>
+                                    <div className="col-md-12 text-md-end">
+                                        <p className="h3 mb-0 p-3 p-md-0">
+                                            Se encontraron {avisos.total} registros para la categoría:
+                                            {cat.nombre}
+                                        </p>
+                                        <hr />
+                                    </div>
+                                </div>
                                 <div className="row mb-4 gy-4">
-                                    {/*avisos.datos.map((aviso) => (
+                                    {avisos.datos.map((aviso) => (
                                         <Aviso key={aviso.id} aviso={aviso} />
-                                    ))*/}
+                                    ))}
 
 
                                 </div>
-                                <nav aria-label="Page navigation example">
-                                    <ul className="pagination justify-content-end mb-0">
-                                        <li className="page-item mx-1">
-                                            <Link
-                                                className="page-link rounded shadow-sm px-3"
-                                                href="/categorias?page=1"
-                                                aria-label="Primera"
-                                                title="Primera"
-                                            >
-                                                <span aria-hidden="true">«««</span>
-                                            </Link>
-                                        </li>
-                                        <li className="page-item mx-1">
-                                            <Link
-                                                className="page-link rounded shadow-sm px-3"
-                                                href={''}
-                                                aria-label="Anterior"
-                                                title="Anterior"
-                                            >
-                                                <span aria-hidden="true">«</span>
-                                            </Link>
-                                        </li>
-                                        {/*paginación numérica */}
-                                        {/*[...paginas].map((x, i) => (
-                                            <li className="page-item mx-1" key={i}>
-                                                {i >= 1 && (
-                                                    <Link
-                                                        className="page-link rounded shadow-sm px-3"
-                                                        href={`/categorias?page=${i}`}
-                                                    >
-                                                        {i}
-                                                    </Link>
-                                                )}
-                                            </li>
-                                        ))*/}
-                                        {/*//paginación numérica */}
-                                        <li className="page-item mx-1">
-                                            <Link
-                                                className="page-link rounded shadow-sm px-3"
-                                                href={``}
-                                                aria-label="Siguiente"
-                                                title="Siguiente"
-                                            >
-                                                <span aria-hidden="true">»</span>
-                                            </Link>
-                                        </li>
-                                        <li className="page-item mx-1">
-                                            <Link
-                                                className="page-link rounded shadow-sm px-3"
-                                                href={``}
-                                                aria-label="Última"
-                                                title="Última"
-                                            >
-                                                <span aria-hidden="true">»»»</span>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </nav>
+
                             </div>
                         </div>
                     </div>
@@ -102,3 +57,35 @@ const AvisosPorCategoria = () => {
 }
 
 export default AvisosPorCategoria
+
+//te dejo la tarea de agregarle paginación
+export async function getServerSideProps({ context, params }) {
+    if (params) {
+        let categorias = await getCategorias();
+        let avisos = await getAvisosPorCategoria(params.slug);
+        let cat = await getCategoriasPorId(params.slug);
+        return {
+            props:
+            {
+                categorias, avisos, cat
+            }
+        }
+    }
+
+}
+
+
+/*
+export async function getServerSideProps(context)
+{
+    let page = context.query.page;
+    let categorias = await getCategorias();
+    let avisos = await getAvisos(page);
+    return{
+        props:
+        {
+            categorias, avisos, page
+        }
+    }
+}
+*/
--- pages/detalle/[...params].js
+++ pages/detalle/[...params].js
@@ -1,17 +1,285 @@
 import Frontend from "@/componentes/Frontend";
 import Link from "next/link";
+import Image from "next/image"
+import { useState  } from "react";
 import { useRouter } from "next/router";
 import Sidebar from "@/componentes/Sidebar";
 import Aviso from "@/componentes/Aviso";
+import { getAvisosPorId, getCategorias, getAvisosComentariosPorId, addAvisosComentariosPorId } from "@/servicios/APIRest";
+import { formatearFecha } from "@/helpers/helper";
 
-const Detalle = () => {
+const Detalle = ({ datos, categorias, comentarios, handleEstaLogueado, authNombre, handleCerrarSesion }) => {
     const router = useRouter()
     console.log(router.query.slug)//aqui se recupera con el nombre del archivo en este caso slug
+
+    const [nombre, setNombre] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [mensaje, setMensaje] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (nombre == 0 || nombre == "") {
+            Swal.fire({
+                icon: "error",
+                title: "Ups",
+                text: "El campo nombre está vacío",
+            });
+            setNombre("");
+            return;
+        }
+        if (correo == 0 || correo == "") {
+            Swal.fire({
+                icon: "error",
+                title: "Ups",
+                text: "El campo E-Mail está vacío",
+            });
+            setCorreo("");
+            return;
+        }
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(correo)) {
+            Swal.fire({
+                icon: "error",
+                title: "Ups",
+                text: "El E-Mail ingresado no es válido",
+            });
+            setCorreo("");
+            return;
+        }
+        if (mensaje == 0 || mensaje == "") {
+            Swal.fire({
+                icon: "error",
+                title: "Ups",
+                text: "El campo mensaje está vacío",
+            });
+            setMensaje("");
+            return;
+        }
+        if (await addAvisosComentariosPorId({ nombre: nombre, correo: correo, clasificados_avisos_id: datos.id, mensaje: mensaje })) {
+            Swal.fire({
+                icon: "success",
+                title: "Ok",
+                text: "Se creó el registro exitosamente",
+            });
+            setNombre("");
+            setCorreo("");
+            setMensaje("");
+            return router.replace(router.asPath);
+        } else {
+            return Swal.fire({
+                icon: "error",
+                title: "Ops",
+                text: "Ocurrió un error, por favor inténtelo otra vez",
+            });
+        }
+    }
+
     return (
         <>
-            hola
+            <Frontend title={`${datos.nombre}`} authNombre={authNombre} handleCerrarSesion={handleCerrarSesion}>
+                <section className="py-5">
+                    <div className="container py-5">
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item">
+                                    <Link href="/">Home</Link>
+                                </li>
+                                <li className="breadcrumb-item">
+                                    <Link href={`/categorias`}>Categorías</Link>
+                                </li>
+                                <li className="breadcrumb-item active" aria-current="page">
+                                    {datos.nombre}
+                                </li>
+                            </ol>
+                        </nav>
+                        <div className="row gy-5">
+                            <div className="col-lg-8">
+                                <div className="card border-0 shadow-sm mb-4 mb-lg-5 p-2 p-lg-0">
+                                    <div className="card-body p-lg-5">
+                                        <p>
+                                            <Image
+                                                className="img-fluid"
+
+                                                src={`${process.env.NEXT_PUBLIC_BASE_URL_LOCAL}uploads/avisos/${datos.foto}`}
+                                                width={640}
+                                                height={280}
+                                                alt={datos.nombre}
+                                                unoptimized={true} priority
+                                            />
+                                        </p>
+                                        <h2 className="h1 mb-4">{datos.nombre}</h2>
+                                        <h2 className="h4 mb-4">Categoría <Link href={`/categoria/${datos.clasificados_categoria_slug}`} title={datos.clasificados_categoria_nombre}>{datos.clasificados_categoria_nombre}</Link></h2>
+                                        <p>{datos.descripcion}</p>
+                                    </div>
+                                </div>
+
+                                <div className="card border-0 shadow-sm p-2 p-lg-0">
+                                    <div className="card-body p-lg-5">
+                                        <h2 className="h3 mb-4">Hay {comentarios.length} Comentarios</h2>
+                                        {comentarios.map((comentario) => (
+                                            <div key={comentario.id} className="mb-4">
+                                                <div className="row mb-3">
+                                                    <div className="col-lg-8">
+                                                        <div className="d-flex align-items-center">
+                                                            <Image
+                                                                className="rounded-circle"
+                                                                src="/img/usuario.png"
+                                                                alt=""
+                                                                height={40}
+                                                                width="40"
+                                                            />
+
+
+
+                                                            <div className="ms-2">
+                                                                <h6 className="mb-0">{comentario.nombre}</h6>
+                                                                <p className="small text-muted mb-0 fw-bold">
+                                                                    {formatearFecha(comentario.fecha)}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <div className="row ps-5">
+                                                    <div className="col-12">
+                                                        <p className="text-sm">
+                                                            {comentario.mensaje}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <a
+                                            className="btn btn-secondary"
+                                            href="#reviewPanel"
+                                            role="button"
+                                            data-bs-toggle="collapse"
+                                            title="Agregar comentario"
+                                        >
+                                            <i className="fas fa-plus"></i> Agregar comentario
+                                        </a>
+                                        <div className="collapse" id="reviewPanel">
+                                            <div className="pt-4">
+                                                <form onSubmit={handleSubmit}>
+                                                    <div className="row gy-3">
+                                                        <div className="col-lg-6">
+                                                            <label className="form-label" htmlFor="nombre">
+                                                                Nombre
+                                                            </label>
+                                                            <input
+                                                                className="form-control form-control-lg"
+                                                                id="nombre"
+                                                                type="text"
+                                                                value={nombre}
+                                                                onChange={(e) => setNombre(e.target.value)}
+                                                                placeholder="Tu nombre:"
+                                                            />
+                                                        </div>
+                                                        <div className="col-lg-6">
+                                                            <label className="form-label" htmlFor="correo">
+                                                                E-Mail
+                                                            </label>
+                                                            <input
+                                                                className="form-control form-control-lg"
+                                                                id="correo"
+                                                                type="text"
+                                                                value={correo}
+                                                                onChange={(e) => setCorreo(e.target.value)}
+                                                                placeholder="Tu E-Mail"
+                                                            />
+                                                        </div>
+                                                        <div className="col-12">
+                                                            <label className="form-label" htmlFor="mensaje">Tu mensaje</label>
+                                                            <textarea className="form-control form-control-lg" id="mensaje" value={mensaje}
+                                                                onChange={(e) => setMensaje(e.target.value)} rows="5" placeholder="Deja tu mensaje aquí."></textarea>
+                                                        </div>
+                                                        <div className="col-12">
+                                                            <button className="btn btn-primary" title="Enviar"><i className="fas fa-comment"></i> Enviar</button>
+                                                        </div>
+                                                        <div className="col-12 d-flex justify-content-start"></div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col-lg-4">
+                                <div className="card border-0 shadow-sm mb-4 mb-lg-5 p-2 p-lg-0">
+                                    <div className="card-body p-4 p-lg-5">
+                                        <h2 className="h3 mb-4">Social links</h2>
+                                        <ul className="list-inline mb-0">
+                                            <li className="list-inline-item">
+                                                <a className="social-link facebook" href="#!">
+                                                    <i className="fab fa-facebook-f"></i>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a className="social-link twitter" href="#!">
+                                                    <i className="fab fa-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a className="social-link vimeo" href="#!">
+                                                    <i className="fab fa-vimeo"></i>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a className="social-link instagram" href="#!">
+                                                    <i className="fab fa-instagram"></i>
+                                                </a>
+                                            </li>
+                                            <li className="list-inline-item">
+                                                <a className="social-link youtube" href="#!">
+                                                    <i className="fab fa-youtube"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div className="card border-0 shadow-sm mb-4 mb-lg-5 p-2 p-lg-0">
+                                    <div className="card-body p-4 p-lg-5">
+                                        <h2 className="h3 mb-4">Categorías</h2>
+                                        <ul className="list-inline mb-0">
+                                            {categorias.map((categoria) => (
+                                                <li key={categoria.id} className="list-inline-item m-1">
+                                                    <Link
+                                                        className={`btn btn-${datos.clasificados_categoria_id === categoria.id ? "outline-danger" : "light"}`}
+                                                        href={`/categoria/${categoria.slug}`}
+                                                        title={categoria.nombre}
+
+                                                    >
+                                                        {categoria.nombre}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </Frontend>
         </>
     )
 }
 
 export default Detalle
+
+export async function getServerSideProps({ params }) {
+    let categorias = await getCategorias();
+    if (params) {
+        let datos = await getAvisosPorId(params.params[0]);
+        let comentarios = await getAvisosComentariosPorId(params.params[0]);
+        return {
+            props: {
+                datos, categorias, comentarios
+            }
+        }
+    }
+}
